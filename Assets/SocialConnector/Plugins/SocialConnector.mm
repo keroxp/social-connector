60cadb6ebda8df911b7504fa441edd93d8f2161f
Yusuke Sakurai <kerokerokerop@gmail.com>
2018-01-09 01:42:24 +0900

    private version

--- Assets/SocialConnector/Plugins/SocialConnector.mm
+++ Assets/SocialConnector/Plugins/SocialConnector.mm
@@ -8,28 +8,162 @@
 #if UNITY_VERSION <= 434
 #import "iPhone_View.h"
 #endif
-extern "C" {
+#import <MessageUI/MessageUI.h>
 
-    void SocialConnector_Share(const char *text, const char *url, const char *textureURL) {
+extern "C" {
     
+    @interface HXMailComposeViewDelegate : NSObject<MFMailComposeViewControllerDelegate>
+@end
+    @implementation HXMailComposeViewDelegate
+- (void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    [UnityGetGLViewController() dismissViewControllerAnimated:YES completion:nil];
+}
+@end
+    @interface SocialActivity : UIActivity
+- (id)initWithTitle:(NSString *)title scheme:(NSString *)scheme imageName:(NSString *)imageName action:(id)myblock;
+- (BOOL)isInstalled;
+@end
+    
+    @implementation SocialActivity
+
+NSString *_title;
+NSString *_scheme;
+NSString *_imageName;
+
+void *(^_myblock)(void);
+
++ (UIActivityCategory)activityCategory {
+    return UIActivityCategoryShare;
+}
+
+- (id)initWithTitle:(NSString *)title scheme:(NSString *)scheme imageName:(NSString *)imageName action:myblock {
+    _title = title;
+    _scheme = scheme;
+    _imageName = imageName;
+    _myblock = [myblock copy];
+    return self;
+}
+
+- (BOOL)isInstalled {
+#if TARGET_IPHONE_SIMULATOR
+    return YES;
+#endif
+    return [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:_scheme]];
+}
+
+- (NSString *)activityTitle {
+    return _title;
+}
+
+- (UIImage *)_activityImage {
+    if ([self isInstalled]) {
+        return [UIImage imageNamed:_imageName];
+    }
+    else {
+        return nil;
+    }
+}
+
+- (BOOL)canPerformWithActivityItems:(NSArray *)activityItems {
+    return YES;
+    
+}
+
+- (void)prepareWithActivityItems:(NSArray *)activityItems; {
+    [super prepareWithActivityItems:activityItems];
+}
+
+- (UIViewController *)activityViewController {
+    return nil;
+}
+
+- (void)performActivity {
+    
+    _myblock();
+    
+    [self activityDidFinish:YES];
+}
+
+- (void)activityDidFinish:(BOOL)completed {
+    [super activityDidFinish:completed];
+}
+@end
+    
+    void SocialConnector_Mail (const char *to, const char *title, const char *message, const char *logfileUrl) {
+        MFMailComposeViewController *picker = [[MFMailComposeViewController alloc] init];
+        auto path = [NSString stringWithCString:logfileUrl encoding:NSUTF8StringEncoding];
+        auto basename = [[path componentsSeparatedByString:@"/"] lastObject];
+        auto file = [NSData dataWithContentsOfFile:[NSString stringWithCString:logfileUrl encoding:NSUTF8StringEncoding]];
+        [picker setToRecipients:@[[NSString stringWithCString:to encoding:NSUTF8StringEncoding]]];
+        [picker addAttachmentData:file mimeType:@"text/plain" fileName:basename];
+        [picker setSubject:[NSString stringWithCString:title encoding:NSUTF8StringEncoding]];
+        [picker setMessageBody:[NSString stringWithCString:message encoding:NSUTF8StringEncoding] isHTML:false];
+        auto del = [[HXMailComposeViewDelegate alloc] init];
+        picker.mailComposeDelegate = del;
+        [UnityGetGLViewController() presentViewController:picker animated:YES completion: nil];
+    }
+    
+    void SocialConnector_Share(const char *text, const char *url, const char *textureURL) {
+        
         NSString *_text = [NSString stringWithUTF8String:text ? text : ""];
         NSString *_url = [NSString stringWithUTF8String:url ? url : ""];
         NSString *_textureURL = [NSString stringWithUTF8String:textureURL ? textureURL : ""];
-    
+        
         UIImage *image = nil;
-    
+        
         if ([_textureURL length] != 0) {
             image = [UIImage imageWithContentsOfFile:_textureURL];
         }
-    
+        
         NSArray *actItems = [NSArray arrayWithObjects:_text, _url, image, nil];
-    
-        UIActivityViewController *activityView = [[[UIActivityViewController alloc] initWithActivityItems:actItems applicationActivities: nil] autorelease];
-    
+        
+        SocialActivity *social = [[[SocialActivity alloc] initWithTitle:@"LINE" scheme:@"line://" imageName:@"LINE" action:^() {
+            
+            NSString *contentType;
+            NSString *contentKey;
+            
+            if ([_textureURL length] != 0) {
+                UIImage *image = [UIImage imageWithContentsOfFile:_textureURL];
+                if (image == nil) {
+                    return;
+                }
+                
+                UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+                pasteboard.image = image;
+                contentType = @"image";
+                contentKey = pasteboard.name;
+                
+            } else if ([_text length] != 0) {
+                contentType = @"text";
+                contentKey = _text;
+                
+                if ([_url length] != 0) {
+                    contentKey = [contentKey stringByAppendingFormat:@" - %@", _url];
+                }
+            }
+            
+            
+            NSString *lineUrlString = [NSString stringWithFormat:@"line://msg/%@/%@", contentType, contentKey];
+            
+            lineUrlString = [lineUrlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+            
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:lineUrlString]];
+            
+        }] autorelease];
+        
+        NSArray *myItems = [NSArray arrayWithObjects:nil, nil];
+        
+        if (social.isInstalled) {
+            myItems = [myItems arrayByAddingObject:social];
+        }
+        
+        UIActivityViewController *activityView = [[[UIActivityViewController alloc] initWithActivityItems:actItems applicationActivities:myItems] autorelease];
+        
         if(floorf(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_7_1)
             activityView.popoverPresentationController.sourceView = UnityGetGLViewController().view;
-    
+        
         [UnityGetGLViewController() presentViewController:activityView animated:YES completion:nil];
     }
-
+    
 }
--- Assets/SocialConnector/SocialConnector.cs
+++ Assets/SocialConnector/SocialConnector.cs
@@ -1,73 +1,84 @@
 using UnityEngine;
 
 #if UNITY_IPHONE
-
 using System.Runtime.InteropServices;
 
 #endif
-
-namespace SocialConnector
+public class SocialConnector
 {
-	public class SocialConnector
-	{
-		#if UNITY_IPHONE
-		[DllImport ("__Internal")]
-		private static extern void SocialConnector_Share (string text, string url, string textureUrl);
-
-
-		#elif UNITY_ANDROID
-		private static AndroidJavaObject clazz = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-		private static AndroidJavaObject activity = clazz.GetStatic<AndroidJavaObject> ("currentActivity");
-		#endif
-
-		#if UNITY_IPHONE
-		
-		private static void _Share (string text, string url, string textureUrl)
-		{
-			SocialConnector_Share (text, url, textureUrl);
-		}
-
-
-		#elif UNITY_ANDROID
-		
-		private static void _Share (string text, string url, string textureUrl)
-		{
-			using (var intent = new AndroidJavaObject ("android.content.Intent")) {
-				intent.Call<AndroidJavaObject> ("setAction", "android.intent.action.SEND");
-				intent.Call<AndroidJavaObject> ("setType", string.IsNullOrEmpty (textureUrl) ? "text/plain" : "image/png");
-
-				if (!string.IsNullOrEmpty (url))
-					text += "\t" + url;
-				if (!string.IsNullOrEmpty (text))
-					intent.Call<AndroidJavaObject> ("putExtra", "android.intent.extra.TEXT", text);
-
-				if (!string.IsNullOrEmpty (textureUrl)) {
-					var uri = new AndroidJavaClass ("android.net.Uri");
-					var file = new AndroidJavaObject ("java.io.File", textureUrl);
-					intent.Call<AndroidJavaObject> ("putExtra", "android.intent.extra.STREAM", uri.CallStatic<AndroidJavaObject> ("fromFile", file));
-				}
-				var chooser = intent.CallStatic<AndroidJavaObject> ("createChooser", intent, "");
-				chooser.Call<AndroidJavaObject> ("putExtra", "android.intent.extra.EXTRA_INITIAL_INTENTS", intent);
-				activity.Call ("startActivity", chooser);
-			}
-		}
-		#endif
-
-		public static void Share (string text)
-		{
-			Share (text, null, null);
-		}
-
-		public static void Share (string text, string url)
-		{
-			Share (text, url, null);
-		}
-
-		public static void Share (string text, string url, string textureUrl)
-		{
-#if UNITY_ANDROID || UNITY_IPHONE
-			_Share (text, url, textureUrl);
+#if UNITY_IOS
+    [DllImport("__Internal")]
+    private static extern void SocialConnector_Share(string text, string url, string textureUrl);
+    [DllImport("__Internal")]
+    private static extern void SocialConnector_Mail(string to, string title, string message, string logFileUrl);
+#elif UNITY_ANDROID
+    private static AndroidJavaObject clazz = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    private static AndroidJavaObject activity = clazz.GetStatic<AndroidJavaObject>("currentActivity");
 #endif
-		}
-	}
+
+#if UNITY_IPHONE
+
+    private static void _Share(string text, string url, string textureUrl)
+    {
+        SocialConnector_Share(text, url, textureUrl);
+    }
+
+#elif UNITY_ANDROID
+
+    private static void _Share(string text, string url, string textureUrl)
+    {
+        using (var intent = new AndroidJavaObject("android.content.Intent"))
+        {
+            intent.Call<AndroidJavaObject>("setAction", "android.intent.action.SEND");
+            intent.Call<AndroidJavaObject>("setType", string.IsNullOrEmpty(textureUrl) ? "text/plain" : "image/png");
+
+            if (!string.IsNullOrEmpty(url))
+                text += "\t" + url;
+            if (!string.IsNullOrEmpty(text))
+                intent.Call<AndroidJavaObject>("putExtra", "android.intent.extra.TEXT", text);
+
+            if (!string.IsNullOrEmpty(textureUrl))
+            {
+                var unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+                var currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+                //var uri = new AndroidJavaClass("android.net.Uri");
+                var file = new AndroidJavaObject("java.io.File", textureUrl);
+                //intent.Call<AndroidJavaObject>("putExtra", "android.intent.extra.STREAM", uri.CallStatic<AndroidJavaObject>("fromFile", file));
+                var fileProvider = new AndroidJavaClass("android.support.v4.content.FileProvider");
+                var uri = fileProvider.CallStatic<AndroidJavaObject>("getUriForFile", currentActivity,
+                    "studio.hexat.arika.fileprovider", file);
+                intent.Call<AndroidJavaObject>("putExtra", "android.intent.extra.STREAM", uri);
+            }
+            var chooser = intent.CallStatic<AndroidJavaObject>("createChooser", intent, "");
+            chooser.Call<AndroidJavaObject>("putExtra", "android.intent.extra.EXTRA_INITIAL_INTENTS", intent);
+            activity.Call("startActivity", chooser);
+        }
+    }
+#endif
+
+    public static void Mail(string to, string title, string message, string logFileUrl)
+    {
+#if UNITY_EDITOR
+#elif UNITY_IOS
+        SocialConnector_Mail(to, title, message, logFileUrl);
+#else
+#endif
+    }
+
+    public static void Share(string text)
+    {
+        Share(text, null, null);
+    }
+
+    public static void Share(string text, string url)
+    {
+        Share(text, url, null);
+    }
+
+    public static void Share(string text, string url, string textureUrl)
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        _Share(text, url, textureUrl);
+#endif
+    }
 }
